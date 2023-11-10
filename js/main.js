970edc913418b97c66f567ee23cd182f6c658823
PinoGabriel <pinogabriel01@gmail.com>
2023-11-10 15:49:37 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+/* Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	            scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+ */
+
+// CREARE UN ARRAY DI OGGETTI CON TUTTE LE INFORMAZIONI
+
+const list = [
+    {
+        userName : "Wayne",
+        userSurname : "Barnett",
+        userWork : "Founder & CEO",
+        img : "./img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        userName : "Angela",
+        userSurname : "Caroll",
+        userWork : "Chief Editor",
+        img : "./img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        userName : "Walter",
+        userSurname : "Gordon",
+        userWork : "Office Manager",
+        img : "./img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        userName : "Angela",
+        userSurname : "Lopez",
+        userWork : "Social Media Manager",
+        img : "./img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        userName : "Scott",
+        userSurname : "Estrada",
+        userWork : "Developer",
+        img : "./img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        userName : "Barbara",
+        userSurname : "Ramos",
+        userWork : "Graphic Designer",
+        img : "./img/wayne-barnett-founder-ceo.jpg"
+    }
+];
+
+key(list);
+
+// STAMPRAE SU CONSOLE PER OGNI MEMBRO LE INFORMAIZONI
+function key(elemento) {
+    
+    for (let i = 0; i < elemento.length; i++) {
+        
+        for (let chiave in elemento[i]) {
+            console.log(chiave, elemento[i][chiave]);
+        }
+        
+    }
+}
+
+
+
+// STAMPARE LE INFO NEL DOM 
+
+const msg = document.getElementById("msg")
